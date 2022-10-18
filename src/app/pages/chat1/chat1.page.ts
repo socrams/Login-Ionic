b479988a95e7bea64b76dcddc3e63369688d17f0
socrams <82241613+socrams@users.noreply.github.com>
2022-10-17 21:17:37 -0300

    chat andando

--- src/app/pages/chat1/chat1.page.ts
+++ src/app/pages/chat1/chat1.page.ts
@@ -1,5 +1,5 @@
+
 import { Component, OnInit } from '@angular/core';
-import { createClient } from '@supabase/supabase-js';
 import { SupabaseService } from 'src/app/services/supabase.service';
 import { environment } from 'src/environments/environment';
 import { LoginPage } from '../login/login.page';
@@ -10,126 +10,136 @@ import { LoginPage } from '../login/login.page';
   styleUrls: ['./chat1.page.scss'],
 })
 export class Chat1Page implements OnInit {
-message:string;
-style: string;
-conversacion : string= '';
+  message:string;
+  style: string;
+  conversacion : string= '';
 x = new Array;
 chats = this.supabaseService.chat;
+valor:boolean=false;
+recargar:any;
 constructor(   
-    private supabaseService : SupabaseService,
+  private supabaseService : SupabaseService,
   ) {
   }
-
-//   EscucharChat(): void{
-//   const supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  
+  async enviarMessage(){
+    this.supabaseService.addMessage(this.message);
+    this.message='';
+  }
+  ngOnInit():void {
+    setTimeout(this.tiempo,1000)
+  }
+  
+  tiempo() {
+    if(this.valor){
+      return;
+    }else{
+      return false;
+    }
+  }
+}
 //   const user = supabase.auth.user();
 // const mySubscription = supabase
 // .from('menssages')
 // .on('*', async payload => {
-//   this.conversacion = payload.new.menssages;
-//   console.log(payload)
-//await this.obtener(payload.new.user);
-//   supabase.auth.user()?.email + ":" + this.conversacion;
-// })
-// .subscribe()
-// const subscriptions = supabase.getSubscriptions()
-//   } 
+  //   this.conversacion = payload.new.menssages;
+  //   console.log(payload)
+  //await this.obtener(payload.new.user);
+  //   supabase.auth.user()?.email + ":" + this.conversacion;
+  // })
+  // .subscribe()
+  // const subscriptions = supabase.getSubscriptions()
+  //   } 
   
-async enviarMessage(){
-    this.supabaseService.addMessage(this.message);
-  }
-  ngOnInit():void {
-  }
-
-  
-}
-/*
-angular.module('ionicApp', ['ionic'])
-
-// All this does is allow the message
-// to be sent when you tap return
-.directive('input', function($timeout) {
-  return {
-    restrict: 'E',
-    scope: {
-      'returnClose': '=',
-      'onReturn': '&',
-      'onFocus': '&',
-      'onBlur': '&'
-    },
-    link: function(scope, element, attr) {
-      element.bind('focus', function(e) {
-        if (scope.onFocus) {
-          $timeout(function() {
-            scope.onFocus();
-          });
-        }
-      });
-      element.bind('blur', function(e) {
-        if (scope.onBlur) {
-          $timeout(function() {
-            scope.onBlur();
-          });
-        }
-      });
-      element.bind('keydown', function(e) {
-        if (e.which == 13) {
-          if (scope.returnClose) element[0].blur();
-          if (scope.onReturn) {
-            $timeout(function() {
-              scope.onReturn();
+  /*
+  angular.module('ionicApp', ['ionic'])
+      
+      // All this does is allow the message
+      // to be sent when you tap return
+      .directive('input', function($timeout) {
+        return {
+          restrict: 'E',
+          scope: {
+            'returnClose': '=',
+            'onReturn': '&',
+            'onFocus': '&',
+            'onBlur': '&'
+          },
+          link: function(scope, element, attr) {
+            element.bind('focus', function(e) {
+              if (scope.onFocus) {
+                $timeout(function() {
+                  scope.onFocus();
+                });
+              }
+            });
+            element.bind('blur', function(e) {
+              if (scope.onBlur) {
+                $timeout(function() {
+                  scope.onBlur();
+                });
+              }
+            });
+            element.bind('keydown', function(e) {
+              if (e.which == 13) {
+                if (scope.returnClose) element[0].blur();
+                if (scope.onReturn) {
+                  $timeout(function() {
+                    scope.onReturn();
+                  });
+                }
+              }
             });
           }
         }
-      });
-    }
-  }
-})
-
-
-.controller('Messages', function($scope, $timeout, $ionicScrollDelegate) {
-
-  $scope.hideTime = true;
-
-  var alternate,
-    isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
-
-  $scope.sendMessage = function() {
-    alternate = !alternate;
-
-    var d = new Date();
-  d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
-
+      })
+      
+      //   EscucharChat(): void{
+        //   const supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+      
+      .controller('Messages', function($scope, $timeout, $ionicScrollDelegate) {
+        
+        $scope.hideTime = true;
+        
+        var alternate,
+        isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+        
+        $scope.sendMessage = function() {
+          alternate = !alternate;
+          
+          var d = new Date();
+    d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+    
     $scope.messages.push({
       userId: alternate ? '12345' : '54321',
       text: $scope.data.message,
       time: d
     });
-
+    
     delete $scope.data.message;
     $ionicScrollDelegate.scrollBottom(true);
-
+    
   };
-
-
+  
+  
   $scope.inputUp = function() {
     if (isIOS) $scope.data.keyboardHeight = 216;
     $timeout(function() {
       $ionicScrollDelegate.scrollBottom(true);
     }, 300);
-
+    
   };
 
   $scope.inputDown = function() {
     if (isIOS) $scope.data.keyboardHeight = 0;
     $ionicScrollDelegate.resize();
   };
-
+  
   $scope.closeKeyboard = function() {
     // cordova.plugins.Keyboard.close();
   };
 
-
+  
   $scope.data = {};
   $scope.myId = '12345';
   $scope.messages = [];
